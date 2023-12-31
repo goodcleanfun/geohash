64b9931109754d0e8c98ff027e1617f10815a25d
Al <albarrentine@gmail.com>
2023-12-31 15:59:26 -0500

    [geohash] adding initial implementation from https://github.com/skeeto/geohash rather than the python geohash one, tested at higher precisions, etc.

---
+++ src/geohash.h
@@ -0,0 +1,19 @@
+#ifndef GEOHASH_H
+#define GEOHASH_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+// Validate and decode a geohash into a lat/lon pair. Returns true on success,
+// or false if the buffer does not contain a valid geohash.
+bool geohash_decode(double *lat, double *lon, const char *buf, size_t len);
+
+// Store a geohash encoding of a lat/lon pair at a given precision in a buffer.
+// Adds a null terminator to the end of the buffer. Returns true on success,
+bool geohash_encode(double lat, double lon, size_t precision, char *buf);
+
+#endif
